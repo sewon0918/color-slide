87074989743442a45ae5796b0bb8c450152cf63e
임세원 <imsewon@imsewon-ui-iMac.local>
2022-02-03 03:30:46 +0900

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useCallback, useEffect } from "react";
 
-function App() {
+interface CanvasProps {
+  width: number;
+  height: number;
+}
+
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+function App({ width, height }: CanvasProps) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(
+    undefined
+  );
+  const [isPainting, setIsPainting] = useState(false);
+
+  const getCoordinates = (event: MouseEvent): Coordinate | undefined => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    return {
+      x: event.pageX - canvas.offsetLeft,
+      y: event.pageY - canvas.offsetTop,
+    };
+  };
+
+  const drawLine = (
+    originalMousePosition: Coordinate,
+    newMousePosition: Coordinate
+  ) => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    if (context) {
+      context.strokeStyle = "red";
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+
+      context.beginPath();
+      context.moveTo(originalMousePosition.x, originalMousePosition.y);
+      context.lineTo(newMousePosition.x, newMousePosition.y);
+      context.closePath();
+
+      context.stroke();
+    }
+  };
+
+  const startPaint = useCallback((event: MouseEvent) => {
+    const coordinates = getCoordinates(event);
+    if (coordinates) {
+      setIsPainting(true);
+      setMousePosition(coordinates);
+    }
+  }, []);
+
+  const paint = useCallback(
+    (event: MouseEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (isPainting) {
+        const newMousePosition = getCoordinates(event);
+        if (mousePosition && newMousePosition) {
+          drawLine(mousePosition, newMousePosition);
+          setMousePosition(newMousePosition);
+        }
+      }
+    },
+    [isPainting, mousePosition]
+  );
+
+  const exitPaint = useCallback(() => {
+    setIsPainting(false);
+  }, []);
+
+  const clearCanvas = () => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    canvas.getContext("2d")!!.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+
+    canvas.addEventListener("mousedown", startPaint);
+    canvas.addEventListener("mousemove", paint);
+    canvas.addEventListener("mouseup", exitPaint);
+    // canvas.addEventListener("mouseleave", exitPaint);
+
+    return () => {
+      canvas.removeEventListener("mousedown", startPaint);
+      canvas.removeEventListener("mousemove", paint);
+      canvas.removeEventListener("mouseup", exitPaint);
+      // canvas.removeEventListener("mouseleave", exitPaint);
+    };
+  }, [startPaint, paint, exitPaint]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto my-10">
+      <canvas
+        ref={canvasRef}
+        height={height}
+        width={width}
+        className="rounded-lg bg-gray-200"
+      />
+      <button className="rounded-lg p-3 mt-3 bg-gray-100" onClick={clearCanvas}>
+        delete
+      </button>
     </div>
   );
 }
+
+App.defaultProps = {
+  width: 800,
+  height: 600,
+};
 
 export default App;
