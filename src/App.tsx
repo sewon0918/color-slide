e7aaba7f27fe26f1c500b1f87a9f4aa61f2147e1
임세원 <sewon0918@kaist.ac.kr>
2022-02-22 03:14:46 +0900

    overscroll again

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,11 @@ import { ColorBar } from "./components/ColorBar";
 import { Canvas } from "./components/Canvas";
 import { ColorProvider, useColorState } from "./context";
 import React, { useRef, useState, useEffect, useCallback } from "react";
-import { SelectColor } from "./components/SelectColor";
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile,
-} from "react-device-detect";
 function App() {
   return (
     <ColorProvider>
-      <div className="overflow-hidden overscroll-none">
+      <div>
         <Canvas />
-        {/* <SelectColor /> */}
       </div>
     </ColorProvider>
   );
